c6fb8b7a5074cdc8e03b60001a7ab0fb88009cb4
Jake Howard <git@theorangeone.net>
2020-12-05 14:44:32 +0000

    Day 1

---
+++ 1-report-repair/report-repair.py
@@ -0,0 +1,15 @@
+from itertools import product
+from math import prod
+
+import sys
+import os
+from pathlib import Path
+
+with Path(__file__).parent.joinpath("data.txt").open() as f:
+    data = list(map(int, f.readlines()))
+
+for repeat in [2, 3]:
+    for vals in product(data, repeat=repeat):
+        if sum(vals) == 2020:
+            print(repeat, prod(vals))
+            break
